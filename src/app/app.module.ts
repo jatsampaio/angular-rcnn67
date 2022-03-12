479f362a0764768deeaf2c16c22407a0c54d39f1
jatsampaio <jat.sampaio@gmail.com>
2022-03-12 16:24:29 +0000

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-
+/*
 @NgModule({
   imports: [
     BrowserModule,
@@ -24,6 +24,23 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     ProductAlertsComponent,
     ProductDetailsComponent
   ],
+  */
+  @NgModule({
+    imports: [
+      BrowserModule,
+      ReactiveFormsModule,
+      RouterModule.forRoot([
+        { path: '', component: ProductListComponent },
+        { path: 'products/:productId', component: ProductDetailsComponent },
+      ])
+    ],
+    declarations: [
+      AppComponent,
+      TopBarComponent,
+      ProductListComponent,
+      ProductAlertsComponent,
+      ProductDetailsComponent,
+    ], 
   bootstrap: [
     AppComponent
   ]
