11c849527b0610df08198b8f23f7363d9f9235e4
jatsampaio <jat.sampaio@gmail.com>
2022-03-14 15:15:23 +0000

    3 files modified, 8 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,9 @@
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
@@ -8,13 +14,19 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-/*
+import { CartComponent } from './cart/cart.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ShippingComponent } from './shipping/shipping.component';
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent },
+      { path: 'shipping', component: ShippingComponent },
     ])
   ],
   declarations: [
@@ -22,34 +34,13 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent
   ],
-  */
-  @NgModule({
-    imports: [
-      BrowserModule,
-      ReactiveFormsModule,
-      RouterModule.forRoot([
-        { path: '', component: ProductListComponent },
-        { path: 'products/:productId', component: ProductDetailsComponent },
-      ])
-    ],
-    declarations: [
-      AppComponent,
-      TopBarComponent,
-      ProductListComponent,
-      ProductAlertsComponent,
-      ProductDetailsComponent,
-    ], 
   bootstrap: [
     AppComponent
   ]
 })
 export class AppModule { }
 
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,12 +1,16 @@
-import { Product } from './products';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './products';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   items: Product[] = [];
-  constructor() { }
+
+  constructor(
+    private http: HttpClient
+  ) {}
   addToCart(product: Product) {
     this.items.push(product);
   }
@@ -19,5 +23,10 @@ export class CartService {
     this.items = [];
     return this.items;
   }
-}
+
+  getShippingPrices() {
+        return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+      }
+    }
+
 
